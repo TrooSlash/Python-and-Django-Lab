415ce3ba13c1074cc3945171d2d6b3add2b96dd0
Student <student@example.com>
2025-10-26 14:57:46 +0300

    Лабораторная работа №5: Создание статьи через форму

--- lab3/blog/articles/views.py
+++ lab3/blog/articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 from .models import Article
 
@@ -13,3 +13,21 @@ def get_article(request, article_id):
         return render(request, 'article.html', {"post": post})
     except Article.DoesNotExist:
         raise Http404
+
+def create_post(request):
+    if not request.user.is_authenticated:
+        return redirect('archive')
+    
+    if request.method == "POST":
+        title = request.POST.get('title')
+        text = request.POST.get('text')
+        
+        if title and text:
+            article = Article.objects.create(
+                title=title,
+                text=text,
+                author=request.user
+            )
+            return redirect('archive')
+    
+    return render(request, 'create_post.html')
