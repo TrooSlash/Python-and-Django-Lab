861989c86990342c436409494be8654aed7a65bf
Student <student@example.com>
2025-10-26 14:31:28 +0300

    Лабораторная работа №3-4: Страница отдельной статьи

--- lab3/blog/blog/urls.py
+++ lab3/blog/blog/urls.py
@@ -21,4 +21,5 @@ from articles import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.archive, name='archive'),
+    path('article/<int:article_id>/', views.get_article, name='get_article'),
 ]
